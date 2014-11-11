aeee983a18a875579c2c6888a5b1c465cc5d8520
sapa0002 <sapa0002@algonquinlive.com>
2014-11-11 00:03:52 -0500

    form
    form

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded",  function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 1000,
+      values: [ 1, 1000 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  });
+
+ 
